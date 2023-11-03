3577ef0b566fe6edf2c1e9885c326b525f167768
Rohit Chakraborty <58523433+Thewhite33@users.noreply.github.com>
2023-11-03 11:31:46 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import {logo} from '../assets';
+
+function Hero() {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
+        <button type='button' className='black_btn' onClick={()=>window.open('')}>GitHub</button>
+
+      </nav>
+      <h1 className='head_text'>
+        Summarize Article with <br className='max-md:hidden'/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading experience with Summarize an Open Source Article Summarizer
+      </h2>
+
+    </header>
+  )
+}
+
+export default Hero
